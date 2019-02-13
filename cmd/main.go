09ea42ebc8a7ac7cf337207c528d5776e99eeb91
Pablo Morelli <pabmorelli@gmail.com>
2019-02-13 21:39:06 +0100

    Added almost all behaviour

--- cmd/main.go
+++ cmd/main.go
@@ -10,24 +10,71 @@ func main() {
 	vehicle := domain.NewVehicle()
 
 	fmt.Println(vehicle.GetCurrentState())
-	err := vehicle.FinishRide(10)
+	err := vehicle.FinishRide(10, domain.EndUser)
 	checkError(err)
 
-	err = vehicle.StartRide()
+	err = vehicle.StartRide(domain.EndUser)
 	checkError(err)
 	fmt.Println(vehicle.GetCurrentState())
 
-	err = vehicle.FinishRide(70)
+	err = vehicle.Ready(domain.Hunter)
 	checkError(err)
 	fmt.Println(vehicle.GetCurrentState())
 
-	err = vehicle.StartRide()
+	err = vehicle.FinishRide(70, domain.EndUser)
 	checkError(err)
 	fmt.Println(vehicle.GetCurrentState())
 
-	err = vehicle.FinishRide(19)
+	err = vehicle.StartRide(domain.EndUser)
 	checkError(err)
 	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.FinishRide(19, domain.EndUser)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.Collect(domain.EndUser)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.Collect(domain.Hunter)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.Drop(domain.EndUser)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.Drop(domain.Hunter)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.Ready(domain.EndUser)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.Ready(domain.Hunter)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.SetBatteryLow(domain.Hunter)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	err = vehicle.SetBatteryLow(domain.Admin)
+	checkError(err)
+	fmt.Println(vehicle.GetCurrentState())
+
+	fmt.Println(vehicle.GetCurrentState())
+
+	fmt.Println(vehicle.GetCurrentState())
+
+	fmt.Println(vehicle.GetCurrentState())
+
+	fmt.Println(vehicle.GetCurrentState())
+
+
+
 
 }
 
--- pkg/domain/vehicle.go
+++ pkg/domain/vehicle.go
@@ -4,55 +4,43 @@ import (
 	"errors"
 	"fmt"
 	"github.com/looplab/fsm"
+	"time"
 )
 
 const (
 	readyState = "ready"
 	ridingState = "riding"
-	lowBatteryState = "lowBattery"
+	batteryLowState = "batteryLow"
+	bountyState = "bounty"
+	collectedState = "collected"
+	droppedState = "dropped"
 )
 
 const (
 	startRideEvent = "startRideEvent"
 	finishRideEvent = "finishRideEvent"
-	lowBatteryEvent = "lowBatteryEvent"
+	batteryLowEvent = "batteryLowEvent"
+	bountyEvent = "bountyEvent"
+	collectedEvent = "collectedEvent"
+	droppedEvent = "droppedEvent"
+	readyEvent = "readyEvent"
+)
+
+type UserRole int
+const (
+	EndUser	UserRole = iota + 1
+	Hunter
+	Admin
 )
 
 type Vehicle struct {
 	battery int
 	fsm *fsm.FSM
+	timeFunc time.Timer
 }
 
 func (v *Vehicle) Battery() int {
 	return v.battery
-}
-
-func (v *Vehicle) GetCurrentState() string {
-	return v.fsm.Current()
-}
-
-func (v * Vehicle) StartRide() error {
-	if v.fsm.Can(startRideEvent) {
-		return v.fsm.Event(startRideEvent)
-	}
-
-	errMsg := fmt.Sprintf("you cannot start a ride right now the current state is %s", v.GetCurrentState())
-	return errors.New(errMsg)
-
-}
-
-func (v * Vehicle) FinishRide(batteryLeft int) error {
-	if v.fsm.Can(finishRideEvent) {
-		v.battery = batteryLeft
-		if v.battery < 20 {
-			return v.fsm.Event(lowBatteryEvent)
-		} else {
-			return v.fsm.Event(finishRideEvent)
-		}
-	}
-
-	errMsg := fmt.Sprintf("you cannot finish a ride right now the current state is %s", v.GetCurrentState())
-	return errors.New(errMsg)
 }
 
 func NewVehicle() *Vehicle {
@@ -63,10 +51,134 @@ func NewVehicle() *Vehicle {
 
 			{Name: startRideEvent, Src: []string{readyState}, Dst: ridingState},
 			{Name: finishRideEvent, Src: []string{ridingState}, Dst: readyState},
-			{Name: lowBatteryEvent, Src: []string{ridingState}, Dst: lowBatteryState},
+			{Name: batteryLowEvent, Src: []string{ridingState}, Dst: batteryLowState},
+			{Name: bountyEvent, Src: []string{readyState, batteryLowState}, Dst: bountyState},
+			{Name: collectedEvent, Src: []string{bountyState}, Dst: collectedState},
+			{Name: droppedEvent, Src: []string{collectedState}, Dst: droppedState},
+			{Name: readyEvent, Src: []string{droppedState}, Dst: readyState},
 		},
-		fsm.Callbacks{ },
+		fsm.Callbacks{
+			"enter_" + batteryLowState: func(e *fsm.Event) {
+				e.FSM.SetState(bountyState)
+			},
+		},
 	)
 
-	return &Vehicle{battery:100, fsm:myFsm}
+	return &Vehicle{battery:100, fsm:myFsm }
+}
+
+func (v *Vehicle) GetCurrentState() string {
+	return v.fsm.Current()
+}
+
+func (v * Vehicle) StartRide(role UserRole) error {
+	var err error = nil
+	switch role {
+	case Hunter:
+		fallthrough
+	case EndUser:
+		if v.fsm.Can(startRideEvent) {
+			err = v.fsm.Event(startRideEvent)
+		} else {
+			errMsg := fmt.Sprintf("you cannot finish a ride right now the current state is %s", v.GetCurrentState())
+			err = errors.New(errMsg)
+		}
+	case Admin:
+		v.fsm.SetState(ridingState)
+	}
+
+	return err
+}
+
+func (v * Vehicle) FinishRide(batteryLeft int, role UserRole) error {
+	var err error = nil
+	switch role {
+	case Hunter:
+		fallthrough
+	case EndUser:
+		if v.fsm.Can(finishRideEvent) {
+			v.battery = batteryLeft
+			if v.battery < 20 {
+				err = v.fsm.Event(batteryLowEvent)
+			} else {
+				err = v.fsm.Event(finishRideEvent)
+			}
+		} else {
+			errMsg := fmt.Sprintf("you cannot finish a ride right now the current state is %s", v.GetCurrentState())
+			err = errors.New(errMsg)
+		}
+	case Admin:
+		v.battery = batteryLeft
+		v.fsm.SetState(readyState)
+	}
+
+	return err
+}
+
+func (v * Vehicle) SetBatteryLow(role UserRole) error {
+	var err error = nil
+	if role == Admin {
+		v.fsm.SetState(batteryLowState)
+		v.fsm.SetState(bountyState)
+	} else {
+		err = errors.New("only admin can set the vehicle on low battery")
+	}
+	return err
+}
+
+
+func (v * Vehicle) Collect(role UserRole) error {
+	var err error = nil
+	switch role {
+	case EndUser:
+		err = errors.New("you cannot collect the vehicle being end user")
+	case Hunter:
+		if v.fsm.Can(collectedEvent) {
+			err = v.fsm.Event(collectedEvent)
+		} else {
+			errMsg := fmt.Sprintf("you cannot collect the vehicle right now the current state is %s", v.GetCurrentState())
+			err = errors.New(errMsg)
+		}
+	case Admin:
+		v.fsm.SetState(collectedState)
+	}
+	return err
+}
+
+func (v * Vehicle) Drop(role UserRole) error {
+	var err error = nil
+	switch role {
+	case EndUser:
+		err = errors.New("you cannot drop the vehicle being end user")
+	case Hunter:
+		if v.fsm.Can(droppedEvent) {
+			err = v.fsm.Event(droppedEvent)
+		} else {
+			errMsg := fmt.Sprintf("you cannot drop the vehicle right now the current state is %s", v.GetCurrentState())
+			err = errors.New(errMsg)
+		}
+	case Admin:
+		v.fsm.SetState(droppedState)
+	}
+	return err
+}
+
+func (v * Vehicle) Ready(role UserRole) error {
+	var err error = nil
+	switch role {
+	case EndUser:
+		err = errors.New("you cannot set the vehicle as ready being end user")
+	case Hunter:
+		if v.fsm.Can(readyEvent) {
+			v.battery = 100
+			err = v.fsm.Event(readyEvent)
+		} else {
+			errMsg := fmt.Sprintf("you cannot set the vehicle ready right now the current state is %s", v.GetCurrentState())
+			err = errors.New(errMsg)
+		}
+	case Admin:
+		v.battery = 100
+		v.fsm.SetState(readyState)
+	}
+	return err
 }
